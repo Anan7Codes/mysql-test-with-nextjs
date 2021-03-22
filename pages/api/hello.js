b93fde9ded9f9cdf07976c9497fc449b85cd40a2
Anandhu <anan07dhu@outlook.com>
2021-03-22 20:01:34 +0400

    Code Done :heavy_check_mark

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DB
+});
+
+con.connect(function(err) {
+  if (err) throw err
+  console.log('Connected')
+ });
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  if(req.method === 'GET') {
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) return res.json({err})
+      return res.json({ result })
+    });
+  }
 }
